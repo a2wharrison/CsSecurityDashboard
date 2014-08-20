c77c3b63a7ea2c14da94b67873d2de7a5be78c97
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-08-19 23:51:50 -0400

    Changed logging output.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,12 +1,9 @@
 // configuration for plugin testing - will not be included in the plugin zip
 
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    appenders {
+		console name:'stdout', layout:pattern(conversionPattern: '%d{DATE} %5p %c{3} %m%n')
+	}
 
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
